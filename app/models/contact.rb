f0f012f133436a33d72919df92e7c0e3152c475c
Pavel Shcheglov <p.scheglov@gmail.com>
2015-09-16 16:38:38 +0300

    add model associations and full name validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,10 @@
 class Contact < ActiveRecord::Base
-  has_many :phones
-  has_many :emails
+  validates_uniqueness_of :first_name, scope: :last_name
+  validates_presence_of :first_name, :last_name
+
+  has_many :phones, dependent: :destroy
+  has_many :emails, dependent: :destroy
+
+  accepts_nested_attributes_for :phones, :emails, :allow_destroy => true
+
 end
